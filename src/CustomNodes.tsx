3c620f2527064050757aebbe8d0faad1b525874a
denise <22denise.lie@gmail.com>
2025-04-03 10:57:59 +0800

    start and end node

--- src/CustomNodes.tsx
+++ src/CustomNodes.tsx
@@ -3,20 +3,20 @@ import { Handle, NodeProps, Position } from "@xyflow/react"
 
 export const StartNode: React.FC<NodeProps> = () => {
     return (
-        <div >
-            {/* Image */}
-            <div className="w-10 h-10 mr-2">
-                {/* <img src="/start_icon.png" alt="Start Icon" className="w-full h-full object-contain" /> */}
+        <div className="flex flex-row border border-solid border-gray-300 rounded-lg px-4 py-4 items-center space-x-3 w-60">
+            {/* Image Placeholder */}
+            <div className="w-10 h-10 bg-green-100 flex items-center justify-center rounded-md">
+                <img src="/start_icon.png" alt="Start Icon" className="fit" />
             </div>
 
             {/* Text */}
             <div>
-                <p className="text-sm font-semibold">Start Node</p>
-                <p className="text-xs text-gray-600">Start</p>
+                <p className="text-green-600 font-bold text-sm">Start Node</p>
+                <p className="text-gray-700 font-semibold text-sm">Start</p>
             </div>
 
-            {/* Handle for connecting edges */}
-            <Handle type="source" position={Position.Bottom} className="bg-blue-500" />
+            {/* Connection Handle */}
+            <Handle type="source" position={Position.Bottom} className="bg-gray-500" />
         </div>
     )
 }
@@ -24,12 +24,8 @@ export const StartNode: React.FC<NodeProps> = () => {
 export const EndNode: React.FC<NodeProps> = () => {
     return (
         <>
-            <div
-                style={{
-
-                }}
-            >
-                <p>END</p>
+            <div className="border border-gray-400 rounded-4xl px-25 py-4 w-60 bg-gray-200">
+                <p className="text-gray-400 text-sm font-semibold">END</p>
             </div>
             <Handle type="source" position={Position.Top} />
         </>
