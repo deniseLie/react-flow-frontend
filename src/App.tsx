514663b6f4e46b2c2c9e85f3c0d4cda5d9cb3488
denise <22denise.lie@gmail.com>
2025-04-03 10:24:29 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { useCallback, useState } from 'react';
-import { applyEdgeChanges, applyNodeChanges, Background, Controls, ReactFlow } from '@xyflow/react';
+import { 
+  addEdge, 
+  Background, 
+  Connection, 
+  Controls, 
+  Edge, 
+  ReactFlow, 
+  useEdgesState, 
+  useNodesState 
+} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { nodeTypes } from './CustomNodes';
 
 
 function App() {
 
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  )
+  
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
       <ReactFlow 
         nodes={nodes} 
         edges={edges}
         nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
       >
         <Background />
         <Controls />
